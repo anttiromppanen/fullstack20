d69cbfb2c517658c7ab71a1f7b1efb5629d35ae7
Antti Romppanen <romppanen.antti@gmail.com>
2020-05-02 10:02:01 +0300

    4.17 tehty

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,18 +1,39 @@
+/* eslint-disable no-underscore-dangle */
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
+  const blogs = await Blog
+    .find({}).populate('user', {
+      username: 1,
+      name: 1,
+      id: 1,
+    })
+
+  response.json(blogs.map((u) => u.toJSON()))
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const { body } = request
+
+  const user = await User.findOne({})
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  })
 
   if (!blog.title && !blog.url) return response.status(400).end()
   if (!blog.likes) blog.likes = 0
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   return response.json(savedBlog.toJSON())
 })
 
