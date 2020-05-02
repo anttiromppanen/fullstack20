d0d522ba4e937665c50dd2d091309640ab03dd84
Antti Romppanen <romppanen.antti@gmail.com>
2020-05-02 07:21:56 +0300

    4.15 tehty

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
 const app = require('../app')
 
 const api = supertest(app)
+
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
   {
@@ -143,6 +146,66 @@ describe('PUT', () => {
 
     const updatedBlogs = await Blog.find({ title: newBlog.title })
     expect(updatedBlogs[0].likes).toBe(newBlog.likes)
+  })
+})
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sanasala', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    let usersAtStart = await User.find({})
+    usersAtStart = usersAtStart.map((u) => u.toJSON())
+
+    const newUser = {
+      username: 'PatJaMat',
+      name: 'Patti Tilanne',
+      password: 'pahattilanteet',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    let usersAtEnd = await User.find({})
+    usersAtEnd = usersAtEnd.map((u) => u.toJSON())
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((u) => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    let usersAtStart = await User.find({})
+    usersAtStart = usersAtStart.map((u) => u.toJSON())
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'Salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    let usersAtEnd = await User.find({})
+    usersAtEnd = usersAtEnd.map((u) => u.toJSON())
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
 })
 
